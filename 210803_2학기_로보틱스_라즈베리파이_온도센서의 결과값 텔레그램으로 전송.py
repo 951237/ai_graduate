4fced1ac5fcd22e3947b917cb2f7230136ed1c56
951237 <44091528+951237@users.noreply.github.com>
2021-08-04 00:04:49 +0900

    Create 210803_2학기_로보틱스_라즈베리파이_온도센서의 결과값 텔레그램으로 전송.py
    - todo : 라즈베리 테스트 전용 봇 생성하기
    - 명령추가

---
+++ 210803_2학기_로보틱스_라즈베리파이_온도센서의 결과값 텔레그램으로 전송.py
@@ -0,0 +1,41 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+import time
+import board
+import adafruit_dht
+import telebot
+
+# 텔레그램 기본 설정
+API_KEY = '1527497770:AAHW0DwF3HUwhxjOG4bOk5C7gXR47fX6Si8'  # 텔레그램봇 api
+bot = telebot.TeleBot(API_KEY)
+chat_id = '428116987'
+dhtDevice = adafruit_dht.DHT22(board.D4)
+
+# todo : menu 명령어로 전체 명령어 보여주기
+
+
+# hello World!
+@bot.message_handler(commands=['pi'])
+def greet(message):
+      bot.reply_to(message, "Hello! Pi world!")
+
+# /tmp 입력시 온도값 전송
+@bot.message_handler(commands=['tmp'])
+def check_temp(message):
+    try:
+        # Print the values to the serial port
+        temperature_c = dhtDevice.temperature
+        temperature_f = temperature_c * (9 / 5) + 32
+        humidity = dhtDevice.humidity
+        result = "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
+                temperature_f, temperature_c, humidity )
+        bot.send_message(message.chat.id, result)
+
+    except RuntimeError as error:
+        bot.send_message(message.chat.id, error.args[0])
+        detDevice.exit()
+    
+bot.polling()
+
+                                                                                                               
